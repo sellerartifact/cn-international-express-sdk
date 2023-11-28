0f9f5d07552d20e34470c131c22fc7f0d0a84b82
invalid w <wangjuesix@gmail.com>
2023-11-28 17:11:30 +0800

    feat(三态)： 对接三态物流

--- packages/santai/src/index.ts
+++ packages/santai/src/index.ts
@@ -1,3 +1,33 @@
-import { myRequest } from '@cn-international-express-sdk/utils';
+import { Recordable, promiseSoap } from '@cn-international-express-sdk/utils';
 
-console.log('myRequest', myRequest);
+export interface SanTaiConfig {
+  app_key: string;
+  app_token: string;
+  user_id: string;
+}
+
+export class SanTai {
+  public config: SanTaiConfig;
+
+  private url: string = 'http://www.sfcservice.com/ishipsvc/web-service?wsdl';
+
+  constructor(config: SanTaiConfig) {
+    this.config = config;
+  }
+
+  async genRequest<T>(action: string, sendData: Recordable) {
+    const commonObj = {
+      HeaderRequest: {
+        appKey: this.config.app_key,
+        token: this.config.app_token,
+        userId: this.config.user_id,
+      },
+    };
+    const res = await promiseSoap<T>(
+      this.url,
+      action,
+      Object.assign(sendData, commonObj),
+    );
+    return res;
+  }
+}
