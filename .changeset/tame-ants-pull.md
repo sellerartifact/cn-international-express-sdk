14293f5a7903bc1b2ff4d9a990d6d7052cdea496
invalid w <wangjuesix@gmail.com>
2023-11-29 15:12:25 +0800

    feat: include cainiao-sdk

---
+++ .changeset/tame-ants-pull.md
@@ -0,0 +1,5 @@
+---
+'cn-international-express-sdk': major
+---
+
+include cainiao-sdk
--- packages/cn-international-express-sdk/src/index.ts
+++ packages/cn-international-express-sdk/src/index.ts
@@ -2,3 +2,4 @@ export * from '@cn-international-express-sdk/santai';
 export * from '@cn-international-express-sdk/yuntu';
 export * from '@cn-international-express-sdk/zhxt';
 export * from '@cn-international-express-sdk/yanwen';
+export * from '@cn-international-express-sdk/cainiao';
