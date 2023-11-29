f4153596f345306e837175fc422d9010e577a1b5
invalid w <wangjuesix@gmail.com>
2023-11-29 09:22:35 +0800

    feat: 对接纵横讯通物流

--- packages/zhxt/src/index.ts
+++ packages/zhxt/src/index.ts
@@ -1,3 +1,29 @@
-export default function () {
-  return 'hello world';
+import { Recordable, postRequest } from '@cn-international-express-sdk/utils';
+
+export interface ZhxtConfig {
+  app_key: string;
+  app_token: string;
+  base_url?: string;
+}
+
+export class Zhxt {
+  public config: ZhxtConfig;
+
+  constructor(config: ZhxtConfig) {
+    this.config = config;
+  }
+
+  async genRequest<T>(action: string, sendData?: Recordable): Promise<T> {
+    const url =
+      this.config.base_url ||
+      'http://order.globleexpress.com:8051/webservice/PublicService.asmx/ServiceInterfaceUTF8';
+    const res: any = await postRequest(url, {
+      appToken: this.config.app_token,
+      appKey: this.config.app_key,
+      serviceMethod: action,
+      paramsJson: sendData ? JSON.stringify(sendData) : '',
+    });
+
+    return JSON.parse(res) as T;
+  }
 }
