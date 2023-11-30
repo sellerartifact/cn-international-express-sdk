2f736bc84057c71699f9b5709b30485c50c1bcd8
invalid w <wangjuesix@gmail.com>
2023-11-30 17:57:19 +0800

    feat(cne): 对接递一国际物流

---
+++ .changeset/loud-coins-brush.md
@@ -0,0 +1,6 @@
+---
+'@cn-international-express-sdk/cne': major
+'@cn-international-express-sdk/fpx': minor
+---
+
+Implement functions
--- packages/cne/src/index.ts
+++ packages/cne/src/index.ts
@@ -1,3 +1,42 @@
-export default function () {
-  return 'hello world';
+import {
+  md5,
+  postJSONRequest,
+  Recordable,
+} from '@cn-international-express-sdk/utils';
+
+export interface CneConfig {
+  app_key: string;
+  app_token: string;
+  base_url?: string;
+}
+
+export class Cne {
+  public config: CneConfig;
+
+  constructor(config: CneConfig) {
+    this.config = config;
+  }
+
+  async genRequest<T>(action: string, sendData: Recordable = {}) {
+    const res = await postJSONRequest(
+      this.config.base_url || 'https://api.cne.com/cgi-bin/EmsData.dll?DoApi',
+      this.genUserSignature(action, sendData),
+    );
+    return res as T;
+  }
+
+  genUserSignature(action: string, sendData: Recordable) {
+    Object.assign(sendData, {
+      RequestName: action,
+      TimeStamp: Date.now(),
+      icID: this.config.app_key,
+    });
+    const md5_str = md5(
+      String(sendData.icID) +
+        String(sendData.TimeStamp) +
+        this.config.app_token,
+    );
+    sendData.MD5 = md5_str;
+    return sendData;
+  }
 }
