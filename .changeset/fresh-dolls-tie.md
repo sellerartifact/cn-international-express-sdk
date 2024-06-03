aecf49d40d8533f177012ed4289b7ad3b6681dbe
invalid w <wangjuesix@gmail.com>
2024-06-03 09:29:38 +0800

    feat(one7feia): 增加one7feia物流打印单号操作

---
+++ .changeset/fresh-dolls-tie.md
@@ -0,0 +1,5 @@
+---
+'@cn-international-express-sdk/one7feia': major
+---
+
+增加 one7feia 物流打印单号操作
--- packages/one7feia/src/index.ts
+++ packages/one7feia/src/index.ts
@@ -1,12 +1,15 @@
 import {
   Recordable,
   postJSONRequest,
+  requestImg,
 } from '@cn-international-express-sdk/utils';
 import { One7feiaConfig } from './state';
 
 export * from './state';
 
 export class One7feia {
+  static defaultBaseUrl: string = 'http://api.17feia.com/eship-api/v1/';
+
   public config: One7feiaConfig;
 
   constructor(config: One7feiaConfig) {
@@ -15,11 +18,20 @@ export class One7feia {
 
   async genRequest<T>(action: string, sendData: Recordable = {}): Promise<T> {
     const baseUrl = `${
-      this.config.base_url || 'http://api.17feia.com/eship-api/v1/'
+      this.config.base_url || One7feia.defaultBaseUrl
     }${action}`;
     sendData.apiName = this.config.app_key;
     sendData.apiToken = this.config.app_token;
     const res = await postJSONRequest(baseUrl, sendData);
     return res as T;
+  }
+
+  async printOrderNumber(waybill_number: string) {
+    const parseBaseUrl = new URL(
+      this.config.base_url || One7feia.defaultBaseUrl,
+    );
+    const url = `${parseBaseUrl.origin}/label-internal/v1/label/internal/order/${waybill_number}/pdf`;
+    const buffer = await requestImg(url, 0);
+    return buffer;
   }
 }
