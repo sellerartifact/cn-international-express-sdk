d0b0d9042f2ac7483855b25227767d8c041819dd
invalid w <wangjuesix@gmail.com>
2024-06-04 12:29:01 +0800

    chore(santai): add get rates func

--- packages/santai/src/index.ts
+++ packages/santai/src/index.ts
@@ -1,4 +1,8 @@
-import { Recordable, promiseSoap } from '@cn-international-express-sdk/utils';
+import {
+  Recordable,
+  promiseSoap,
+  promiseStrongSoap,
+} from '@cn-international-express-sdk/utils';
 import { SanTaiConfig } from './state';
 
 export * from './state';
@@ -10,6 +14,29 @@ export class SanTai {
 
   constructor(config: SanTaiConfig) {
     this.config = config;
+  }
+
+  async getRates(sendData: Recordable) {
+    const { country, state, zip_code } = sendData;
+    let { weight, length, width, height } = sendData;
+    width = width || '10';
+    height = height || '10';
+    length = length || '10';
+    weight = weight || 1;
+    const obj = {
+      ratesRequestInfo: {
+        country,
+        state,
+        weight,
+        zip_code,
+        length,
+        width,
+        height,
+        priceType: '1',
+      },
+    };
+    const result = await promiseStrongSoap(this.url, 'getRates', obj);
+    return result;
   }
 
   async genRequest<T>(action: string, sendData: Recordable) {
